fdf196e3051d98e104f269ad07319821eea04294
wangchuang <wangchuang.space@gmail.com>
2023-06-20 09:45:48 +0800

    更新简历

--- src/pages/remoteCV/components/Technology.tsx
+++ src/pages/remoteCV/components/Technology.tsx
@@ -6,21 +6,22 @@ export const Technology = () => {
         <CardTitle title={'专业能力'} />
         <div className='text-sm'>
             <div className='flex items-center my-2'>
-                <span className='round mr-3' />
-                掌握&nbsp;<code>HTML</code>、<code>CSS</code>、<code>JavaScript</code>、<code>ES6</code>语法。
+                <div className='round mr-3' />
+                <div>掌握&nbsp;<code>HTML</code>、<code>CSS</code>、<code>JavaScript</code>、<code>ES6</code>语法。</div>
             </div>
             <div className='flex items-center my-2'>
                 <span className='round mr-3' />
-                {/*熟悉&nbsp;<code>Vue</code>、<code>React</code>、<code>Webpack</code> 等前端技术。*/}
-                熟悉主流前端技术，包括 <code>Vue</code>、<code>React</code>&nbsp;和&nbsp;<code>Webpack</code>，具备在项目中应用这些技术进行开发和优化的经验
+                <div>
+                    熟悉主流前端技术，包括<code>Vue</code>、<code>React</code>&nbsp;和&nbsp;<code>Webpack</code>，具备在项目中应用这些技术进行开发和优化的经验
+                </div>
             </div>
             <div className='flex items-center my-2 break-words'>
                 <span className='round mr-3' />
-                熟练使用&nbsp;<code>Element-UI</code>、<code>Ant Design</code>、<code>Echarts</code>&nbsp;等组件库，能够高效的构建界面和数据可视化。
+                <div>熟练使用&nbsp;<code>Element-UI</code>、<code>Ant Design</code>、<code>Echarts</code>&nbsp;等组件库，能够高效的构建界面和数据可视化。</div>
             </div>
             <div className='flex items-center my-2 break-words'>
                 <span className='round mr-3' />
-                对 &nbsp; <code>Node.js</code>、<code>Vue3</code>、<code>Vite</code>、<code>TypeScript</code>、<code>Tailwindcss</code>、微信小程序，具备一定的了解和实践经验。
+                <div>对 &nbsp; <code>Node.js</code>、<code>Vue3</code>、<code>Vite</code>、<code>TypeScript</code>、<code>Tailwindcss</code>、微信小程序，具备一定的了解和实践经验。</div>
             </div>
         </div>
     </div>
