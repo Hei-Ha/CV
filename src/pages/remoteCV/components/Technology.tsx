81d66ec01857acd49bd7105f2bc852ca3fc00ed0
wangchuang <wangchuang.space@gmail.com>
2023-09-12 11:01:22 +0800

    更新nextjs

--- src/pages/remoteCV/components/Technology.tsx
+++ src/pages/remoteCV/components/Technology.tsx
@@ -17,11 +17,11 @@ export const Technology = () => {
             </div>
             <div className='flex items-center my-2 break-words'>
                 <span className='round mr-3' />
-                <div>熟练使用&nbsp;<code>Element-UI</code>、<code>Ant Design</code>、<code>Echarts</code>&nbsp;等组件库，能够高效的构建界面和数据可视化。</div>
+                <div>熟练使用&nbsp;<code>Element-UI</code>、<code>Ant Design</code>、<code>Tailwindcss</code>、<code>Echarts</code>&nbsp;等组件库，能够高效的构建界面和数据可视化。</div>
             </div>
             <div className='flex items-center my-2 break-words'>
                 <span className='round mr-3' />
-                <div>对 &nbsp; <code>Node.js</code>、<code>Vue3</code>、<code>Vite</code>、<code>TypeScript</code>、<code>Tailwindcss</code>、微信小程序，具备一定的了解和实践经验。</div>
+                <div>对 &nbsp; <code>Node.js</code>、<code>Vue3</code>、<code>Vite</code>、<code>TypeScript</code>、<code>Next.js</code>、微信小程序，具备一定的了解和实践经验。</div>
             </div>
         </div>
     </div>
